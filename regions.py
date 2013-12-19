8bb89170e9e03efa768590c857622cd7e2a175cb
Alexander Ignatenko <avi.cpp@gmail.com>
2013-12-19 21:18:19 +0200

    Possibly right solution

--- regions.py
+++ regions.py
@@ -1,16 +1,19 @@
-map = [
-  [4, 4, 4, 4],
-  [4, 4, 3, 3],
-  [5, 4, 4, 4],
-  [2, 4, 4, 3],
-  ]
+MAP = [
+#  0  1  2  3
+#  [4, 4, 4, 4], # 0
+#  [4, 4, 3, 3], # 1
+#  [5, 4, 3, 3], # 2
+#  [2, 4, 4, 4], # 3
 
-#  [5, 4, 4],
-#  [3, 4, 3],
-#  [5, 5, 3],
-#  [2, 5, 4],
-#  [4, 3, 0],
-#  [3, 3, 2]
+#  0  1  2
+  [5, 4, 4], # 0
+  [3, 4, 3], # 1
+  [5, 5, 3], # 2
+  [2, 5, 4], # 3
+  [4, 3, 0], # 4
+  [3, 3, 2]  # 5
+
+    ]
 
 def to_row(cols, idx):
     return idx / cols
@@ -22,8 +25,8 @@ def to_idx(cols, row, col):
     return cols * row + col
 
 visited = []
-rows = len(map)
-cols = len(map[0])
+rows = len(MAP)
+cols = len(MAP[0])
 areas = 0
 calls = 0
 
@@ -35,30 +38,41 @@ def traversal(row, col):
     if to_idx(cols, row, col) in visited:
         return
 
-    val = map[row][col]
+    visited.append(to_idx(cols, row, col))
+
+    val = MAP[row][col]
     west = None
+    east = None
     south = None
     if col < cols - 1:
-        west = map[row][col + 1]
+        west = MAP[row][col + 1]
+    if col > 0:
+        east = MAP[row][col - 1]
     if row < rows - 1:
-        south = map[row + 1][col]
+        south = MAP[row + 1][col]
 
     if val == west and to_idx(cols, row, col + 1) not in visited:
         traversal(row, col + 1)
-    elif val == south and to_idx(cols, row + 1, col) not in visited:
+    # for traversal element at left-bottom position
+    if val == east and to_idx(cols, row, col - 1) not in visited:
+        traversal(row, col - 1)
+    if val == south and to_idx(cols, row + 1, col) not in visited:
         traversal(row + 1, col)
-    else:
-        areas += 1
 
-    visited.append(to_idx(cols, row, col))
+#    visited.append(to_idx(cols, row, col))
     return
 
 if __name__ == '__main__':
     for row in xrange(rows):
         for col in xrange(cols):
-            traversal(row, col)
+            if to_idx(cols, row, col) not in visited:
+                areas += 1
+                visited.append(None) #tmp
+                traversal(row, col)
+
     print "areas: %d" % areas
     print "visited idx: %s" % visited
-    print "visited [row, col]: %s" % [[to_row(cols, x), to_col(cols, x)] for x in visited]
+    print map(lambda x: [to_row(cols, x), to_col(cols, x)] if x is not None else None, visited)
     print "calls: %d" % calls
+
 
